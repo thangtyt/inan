c3773cdb3f7b87e9dabfc7c587049b9d328e6408
Nguyễn Văn Thắng <thangtyt@gmail.com>
2016-11-24 14:16:48 +0700

    fix url icons

--- features/examination/frontend/controllers/subject.js
+++ features/examination/frontend/controllers/subject.js
@@ -6,11 +6,14 @@ module.exports = function (controller,component,app) {
 
     controller.getSubjects = function (req, res) {
         let actions = app.feature.examination.actions;
+        let host = req.protocol + '://'+req.get('host');
         actions.sFindAll()
         .then(function (subjects) {
                 subjects = subjects.filter(function (val) {
                     if(typeof val.icons == 'string'){
                         val.icons = JSON.parse(val.icons);
+                        val.icons.default = host+val.icons.default;
+                        val.icons.hover = host+val.icons.hover;
                     }
                     return val;
                 })
