fe030cb562d1ea0aed71ea025eb6aa88cec0ce5f
Nguyễn Văn Thắng <thangtyt@gmail.com>
2016-11-24 10:18:11 +0700

    fix jwt

--- library/js_utilities/jwt.js
+++ library/js_utilities/jwt.js
@@ -5,21 +5,28 @@ let jwt = require('jsonwebtoken');
 exports.jwt = function (req,res,next) {
     let jwt_conf = this.getConfig('jwt');
     //let token = req.body.token || req.query.token || req.headers['x-access-token'];
-    let token = req.headers['authorization'].split(' ').pop();
-        if(token){
-            jwt.verify(token,jwt_conf.jwtSecretKey, function (err,decoded) {
-                if(err||!decoded){
-                    res.status(440).jsonp({
-                        messgage: 'Wrong token'
-                    })
-                }else{
-                    req.user = decoded.data;
-                    next();
-                }
-            })
-        }else{
-            res.status(499).jsonp({
-                messgage: 'Token required !'
-            })
-        }
+    let token ;
+    try{
+        token = req.headers['authorization'].split(/[ +~!@#$%^&*()-]/ig).pop();
+    }catch(err){
+        console.log(err);
+        token = null;
+    }
+
+    if(token){
+        jwt.verify(token,jwt_conf.jwtSecretKey, function (err,decoded) {
+            if(err||!decoded){
+                res.status(440).jsonp({
+                    messgage: 'Wrong token'
+                })
+            }else{
+                req.user = decoded.data;
+                next();
+            }
+        })
+    }else{
+        res.status(499).jsonp({
+            messgage: 'Token required !'
+        })
+    }
 }
