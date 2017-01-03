60eab5b6733633633b50b749bf60adfc395bc30a
Nguyễn Văn Thắng <thangtyt@gmail.com>
2017-01-03 14:09:08 +0700

    fix load sections

--- themes/backend/adminLTE/dist/js/exam.js
+++ themes/backend/adminLTE/dist/js/exam.js
@@ -4,37 +4,44 @@
 let content = [];
 
 let sectionDiv = `
-                <div class="box box-info collapsed-box" id="$id$">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">$section-title$</h3>
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="" data-original-title="Collapse">
-                                <i class="fa fa-plus"></i></button>
-                        </div>
-                    </div>
-                    <div class="box-body" style="display: none;">
-                       <div class="form-group">
-                                    <label>Question</label>
-                                    <div class="form-group">
-                                        <select multiple="multiple" id="question-$id$" style="width: 100%" class="form-control"></select>
-                                    </div>
-                                </div>
 
 
-                    </div>
-                    <!-- /.box-body -->
-                    <div class="box-footer" style="display: none;">
-                       <span class="pull-left">Number of questions : </span><span class="pull-right" id='secTotalQues'></span>
-                        <span class="pull-right">Total time : </span><span class="pull-left" id='secTotalTime'></span>
-                    </div>
-                    <!-- /.box-footer-->
-                </div>`;
+                                                <div class="box box-info collapsed-box " id="$id$">
+                                                    <div class="box-header with-border ui-state-default">
+                                                        <h3 class="box-title">$section-title$</h3>
+                                                        <div class="box-tools pull-right">
+                                                            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="" data-original-title="Collapse">
+                                                                <i class="fa fa-plus"></i></button>
+                                                        </div>
+                                                    </div>
+                                                    <div class="box-body" style="display: none;">
+                                                       <div class="form-group">
+                                                                    <label>Question</label>
+                                                                    <div class="form-group">
+                                                                        <select multiple="multiple" id="question-$id$" style="width: 100%" class="form-control"></select>
+                                                                    </div>
+                                                                </div>
+
+
+                                                    </div>
+                                                    <!-- /.box-body -->
+                                                    <div class="box-footer" style="display: none;">
+                                                       <span class="pull-left">Number of questions : </span><span class="pull-right" id='secTotalQues'></span>
+                                                        <span class="pull-right">Total time : </span><span class="pull-left" id='secTotalTime'></span>
+                                                    </div>
+                                                    <!-- /.box-footer-->
+                                                </div>
+
+
+`;
+
+
 
 function loadSections(){
     var selectValue = [];
     var subjectId = $('select[name="subject_id"]').val();
     $.ajax({
-        url: '/admin/examination/exam/list-section/'+subjectId,
+        url: '/admin/exam/list-section/'+subjectId,
         type: 'GET'
     }).done(function(results){
         if(!results) results =[];
@@ -67,9 +74,9 @@ function addSection(element) {
     //console.log('addSection',JSON.stringify(element,2,2));
     var contentDiv = sectionDiv.replace(/\$id\$/g,element.id);
     contentDiv = contentDiv.replace(/\$section-title\$/g,element.text);
-    $('#examDetail').append(contentDiv);
+    $('#sortable').append(contentDiv);
     $.ajax({
-        url: '/admin/examination/exam/list-question/'+element.id,
+        url: '/admin/exam/list-question/'+element.id,
         type: 'GET'
     }).done(function(questions){
         if(!questions) questions =[];
@@ -90,7 +97,7 @@ function addSection(element) {
 
         //for update exam
         var _question = [];
-        //console.log(JSON.stringify(_content,2,2));
+
         if( _content ){
             _content.map(function (con) {
                 if (con.section_id == element.id){
@@ -100,25 +107,29 @@ function addSection(element) {
 
                 }
             });
-            //console.log('update',JSON.stringify(_question,2,2));
-            //$('#question-'+element.id).val(_question).trigger();
-            //console.log(_question);
 
             $('#question-'+element.id).select2('val',_question);
 
             _question.map(function (ques) {
-                //console.log(ques);
-                //$('#question-'+element.id).select2('val',ques).trigger('change');
                 addElement(element.id,ques);
             })
-        }
+        };
+        $("div.sortable").sortable();
+        //sort.sortable({
+        //    placeholder: "ui-sortable-placeholder",
+        //    cancel: ".sort-box-title",
+        //    stop: function (event, ui) {
+        //        data = sort.sortable('serialize', {expression: /(.+)[-](.+)/});
+        //    }
+        //});
+        //sort.disableSelection();
     });
+
 }
 function removeSection(element){
     $('#'+element.id).remove();
 }
 function addElement(section_id,question_id){
-    //console.log(section_id,question_id,content);
     if(question_id == null){
         content.push({
             section_id: section_id,
@@ -129,14 +140,8 @@ function addElement(section_id,question_id){
             if(con.section_id == section_id){
                 con.questions.push(question_id);
             }
-        })
-        //for(var i = 0 ; i < content.length ; i++){
-        //    if(content[i].section_id == section_id){
-        //        content[i].questions.push(question_id);
-        //    }
-        //}
+        });
     }
-    //console.log(JSON.stringify(content,2,2));
 }
 function removeElement(section_id,question_id){
     if(question_id == null){
